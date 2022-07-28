e9c65a28b226634763230dc79b47e38ca1a033a1
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-07-28 13:36:06 -0300

    exercícios concluídos

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const filmes = ["Constantine", "De Volta ao jogo", "John Wick 2"];
+const Data_lancamento = [2005, 2014, 2017];
+const array = [23, "agosto", 1989, true];
+console.log(filmes);
+console.log(Data_lancamento);
+console.log(array);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const filmes = ["Constantine", "De Volta ao jogo", "John Wick 2"];
+const Data_lancamento = [2005, 2014, 2017];
+const array = [23, "agosto", 1989, true];
+
+console.log(filmes[0]);
+console.log(Data_lancamento[1]);
+console.log(array[2]);
+
+console.log(filmes.includes("Constantine"));
+console.log(array.includes("setembro"));
